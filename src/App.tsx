4463bb089c090edc48afbce4d7b2db53fb655d59
js <js@jsui-noteubug.local>
2023-03-07 16:39:08 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { TopMenu } from "./components/TopMenu";
+import { Main } from "./screens/main";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <TopMenu />
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/TopMenu.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/components/topmenu.scss";
+
+export const TopMenu = () => {
+  return (
+    <div className="top">
+      <div className="logo">쌀</div>
+      <div className="topmenu">
+        <div className="menu">쌀효능</div>
+        <div className="menu">SHOP</div>
+        <div className="menu">text</div>
+        <div className="menu">text</div>
+      </div>
+    </div>
+  );
+};
